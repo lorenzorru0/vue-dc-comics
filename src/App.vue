9ae40f05448763b14754c4497d495578deb04292
lorenzorru0 <lorenzoorru999@gmail.com>
2021-09-29 16:41:06 +0200

    Add header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <main>
+
+    </main>
+    <Footer/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    Header,
+    Footer
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import url(assets/scss/common.scss);
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,110 @@
+<template>
+    <header class="container">
+        <div>
+            <img src="../assets/img/dc-logo.png" alt="Logo image">
+        </div>
+        <div>
+            <ul>
+                <li v-for="(element, index) in nav" :key="index"> <a :class="{active : element.active}" :href="element.link">{{element.name}}</a> </li>
+            </ul>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    data() {
+        return {
+            nav: {
+                characters: {
+                    name: 'CHARACTERS',
+                    link: '#',
+                    active: false
+                },
+                comics: {
+                    name: 'COMICS',
+                    link: '#',
+                    active: true
+                },
+                movies: {
+                    name: 'MOVIES',
+                    link: '#',
+                    active: false
+                },
+                tv: {
+                    name: 'TV',
+                    link: '#',
+                    active: false
+                },
+                games: {
+                    name: 'GAMES',
+                    link: '#',
+                    active: false
+                },
+                collectibles: {
+                    name: 'COLLECTIBLES',
+                    link: '#',
+                    active: false
+                },
+                videos: {
+                    name: 'VIDEOS',
+                    link: '#',
+                    active: false
+                },
+                fans: {
+                    name: 'FANS',
+                    link: '#',
+                    active: false
+                },
+                news: {
+                    name: 'NEWS',
+                    link: '#',
+                    active: false
+                },
+                shop: {
+                    name: 'SHOP',
+                    link: '#',
+                    active: false
+                }
+            }
+            }
+            
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import url(../assets/scss/variables.scss);
+
+header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    img {
+        width: 5rem;
+    }
+
+    ul {
+        display: flex;
+        list-style: none;
+
+        li {
+            padding: 0 .9375rem;
+        }
+
+        a {
+            display: inline-block;
+            text-decoration: none;
+            color: #000;
+            padding: 3.125rem 0;
+
+            &.active {
+                color: $commonBlue;
+                border-bottom: .4375rem solid $commonBlue;
+            }
+        }
+    }
+}
+</style>

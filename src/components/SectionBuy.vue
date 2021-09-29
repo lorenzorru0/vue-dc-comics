6fc7e22323cacb9f6514065c1c7f26d3f5cdf343
lorenzorru0 <lorenzoorru999@gmail.com>
2021-09-29 17:51:45 +0200

    Add all main

--- src/components/SectionBuy.vue
+++ src/components/SectionBuy.vue
@@ -3,7 +3,7 @@
         <div class="container">
             <ul>
                 <li v-for="(element, index) in elements" :key="index" >
-                    <img :src="'../img/' + element.src + '.png'" :alt="element.name">
+                    <img :src="'../img/' + element.src" :alt="element.name">
                     <span>{{element.name}}</span>
                 </li>
             </ul> 
@@ -19,31 +19,31 @@ export default {
             elements: {
                 digitalComics: {
                     name: 'DIGITAL COMICS',
-                    src: 'buy-comics-digital-comics',
+                    src: 'buy-comics-digital-comics.png',
                     link: '#',
                     active: false
                 },
                 dcMerchandise: {
                     name: 'DC MERCHANDISE',
-                    src: 'buy-comics-merchandise',
+                    src: 'buy-comics-merchandise.png',
                     link: '#',
                     active: false
                 },
                 subscription: {
                     name: 'SUBSCRITION',
-                    src: 'buy-comics-subscriptions',
+                    src: 'buy-comics-subscriptions.png',
                     link: '#',
                     active: false
                 },
                 comicShopLocator: {
                     name: 'DIGITAL COMICS',
-                    src: 'buy-comics-shop-locator',
+                    src: 'buy-comics-shop-locator.png',
                     link: '#',
                     active: false
                 },
                 dcPowerVisa: {
                     name: 'DC POWER VISE',
-                    src: 'buy-dc-power-visa',
+                    src: 'buy-dc-power-visa.svg',
                     link: '#',
                     active: false
                 },
@@ -61,10 +61,23 @@ section {
 
     ul {
         @include dFlex;
+        justify-content: space-evenly;
         @include list;
+        padding: 4rem 0;
 
         li {
             @include dFlex;
+            align-items: center;
+            color: #fff;
+
+            img {
+                height: 4rem;
+                margin-right: .625rem;
+            }
+
+            &:last-child img {
+                height: 3rem;
+            }
         }
     }
 }
